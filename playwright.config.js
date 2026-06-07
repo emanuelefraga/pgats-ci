51feab8a4a415edf61a4b6b8e1d923fbf1e44b1f
emanuelefraga <contatomanufraga@gmail.com>
2026-06-07 19:37:34 -0300

    correção self-hosted

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: false }],
-    ['junit', { outputFile: `results.xml` }],
+    ['junit', { outputFile: 'results.xml' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
